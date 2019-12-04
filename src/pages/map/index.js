92c71389e2a1ef2dc00c3aa0cfcc8572bedbc014
embermist <caseyy@mapsync.com>
2019-12-03 21:35:39 -0500

    add map

--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -3,7 +3,7 @@ import Layout from '../../components/Layout'
 
 export default class Index extends React.Component {
   componentDidMount() {
-    document.getElementById("map").src = "https://geosync.cloud/maps/2c011f43-c82e-4ba4-aa2f-46d007bcaf72" + this.props.location.search;
+    document.getElementById("map").src = "https://geosync.cloud/maps/7decc866-80aa-4e02-98c7-1d9bc0a6a996" + this.props.location.search;
   }
 
   render() {
